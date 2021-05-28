435e07d5e061f9ab7c2fdc5f54a859f2d66d013e
Jero Buencuerpo <>
2021-05-28 12:14:59 -0600

    added a get_datagraphs function to extract the info from graphs

--- gwyfile/util.py
+++ gwyfile/util.py
@@ -16,3 +16,21 @@ def get_datafields(obj):
         v: obj['/{chnum}/data'.format(chnum=k)]
         for k, v in find_datafields(obj)
     }
+
+def get_datagraphs(obj):
+    """ Returns a dictionary containing the graphs. 
+    Each graph is built as a list of dictionaries of the curves plot. 
+    (only data and description, colors are lost)
+    """
+    token = '/graph/graph/'
+    graphs = [k for k in obj.keys() if k[:-1].endswith(token)]
+
+    ographs = {}
+    for g in graphs:
+        gname = '_'.join(g.split('/')[-2:])
+        curves = []
+        for c in obj[g]['curves']:
+            curve = {k:c[k] for k in ("description", "xdata", "ydata")}
+            curves.append(curve)
+        ographs[gname] =  curves
+    return ographs
